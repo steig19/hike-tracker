2e4fd3b5c7479c20a538709dab639fb3f994a2a5
steig19 <steig@mac.com>
2026-07-15 20:25:01 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -21,7 +21,7 @@
 
   const TRAIL_TOTAL_MI = 2655.8; // Pacific Crest Trail
   
-  const TOTAL_TRAIL_MI = 1269.6; // Current trail miles hiked
+  const TOTAL_TRAIL_MI = 1288.3; // Current trail miles hiked
   
   function fmtDate(ts) {
     try { return new Date(ts).toLocaleString(); }
